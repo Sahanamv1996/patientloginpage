1e2a1a17556124ddeac9f9512b99e26de2bc4370
sahanamv1996 <sahanamn1996@gmail.com>
2020-12-19 17:45:39 +0530

    fstcommit

--- src/app/patient/patient-login.component.ts
+++ src/app/patient/patient-login.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
     selector:'patient-login',
     templateUrl:'./patient-login.component.html'
 })
-
+//developed by sahana on 18-12-2020
 export class PatientLoginComponent implements OnInit{
+    //implement your code here by last approch...
+    loginForm:any;
+    //
+    constructor(private formBuilder : FormBuilder){
+      this.loginForm =  formBuilder.group({
+            userName:['',[]],
+            password:['',[]]
+        });
+    }
     ngOnInit():void{
 
     }
 }
+//ok
